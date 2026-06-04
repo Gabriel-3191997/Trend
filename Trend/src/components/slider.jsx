788f88b135156a12032ff445ca372f1a056511b9
Gabriel W. Kun <gabrielwkun@gmail.com>
2026-06-04 18:32:48 +0000

    deploy

--- Trend/src/components/slider.jsx
+++ Trend/src/components/slider.jsx
@@ -9,15 +9,24 @@ class Slider extends React.Component{
 
             <>
             
-            <div className="md:mt-28 flex flex-nowrap justify-center gap-2">
-                <div className="md:w-96 h-112 md:h-112">
+            <div className="md:mt-28 flex flex-nowrap justify-center gap-2  md:h-auto cursor-pointer">
+                <div className="md:w-96 h-112 md:h-112 relative group overflow-hidden">
                     <img src={img1} alt="TeddyRide" className="object-cover w-96 md:h-full h-full mt-16" />
+                    <div className="absolute inset-0 mt-16 bg-black/50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity  duration-300">
+                        <span className="text-white text-xl font-semibold text-center px-4">TeddyRide</span>
+                    </div>
                 </div>
-                <div className="md:w-96 h-128 bg-black">
+                <div className="md:w-96 h-128 bg-black relative group overflow-hidden cursor-pointer">
                      <img src={img2} alt="TeddyRide" className="object-cover w-96 md:h-full h-full" />
+                     <div className="absolute inset-0 bg-black/50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                        <span className="text-white text-xl font-semibold text-center px-4">JyZno</span>
+                    </div>
                 </div>
-                 <div className="md:w-96 h-112 bg-black">
-                    <img src={img1} alt="TeddyRide" className="object-cover w-96 md:h-full h-full mt-16" />
+                 <div className="md:w-96 h-112 bg-black relative group overflow-hidden cursor-pointer">
+                    <img src={img1} alt="TeddyRide" className="object-cover w-96 md:h-full h-full mt-16 " />
+                    <div className="absolute inset-0 mt-16 bg-black/50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                        <span className="text-white text-xl font-semibold text-center px-4">Artist Three</span>
+                    </div>
                 </div>
             </div>
             
